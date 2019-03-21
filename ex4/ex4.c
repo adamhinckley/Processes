f9cf65d242f6ceb3949ae02fb98a985ca0ddec36
adamhinckley <adamhinckley@mac.com>
2019-03-21 13:08:05 -0600

    ex4 done. . .I think :)

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,7 +1,10 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
+// to run the program `/bin/ls`. Try a few variants of `exec()`, such as
+// `execl()`, `execle()`, `execv()`, and others. Why do you think there
 // are so many variants of the same basic call?
+
+// I found this resource to better understand exec()
+// https://stackoverflow.com/questions/5769734/what-are-the-different-versions-of-exec-used-for-in-c-and-c
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,7 +13,9 @@
 
 int main(void)
 {
-    // Your code here    
+    char *args[] = {"bin/ls", NULL};
+    fork();
+    execv(args[0], args);
 
     return 0;
 }
